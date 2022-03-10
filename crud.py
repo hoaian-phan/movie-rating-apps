e9e51c72e6cf32f5c21f2551d51e0ac4612d14a5
Hoai An Phan <pnhoaian2904@gmail.com>
2022-03-10 23:44:36 +0000

    Completed part 4

--- crud.py
+++ crud.py
@@ -11,6 +11,44 @@ def create_user(email, password):
     return user
 
 
+def show_all_users():
+    """ Return a list of all users """
+
+    users = User.query.all()
+
+    return users
+
+
+def user_by_id(user_id):
+    """ Return a user by their id """
+
+    user = User.query.get(user_id)
+
+    return user
+
+
+def get_user_by_email(email):
+    """ Return a user by their email, if not existed, return None """
+
+    user = User.query.filter(User.email==email).first()
+
+    return user
+
+
+def get_password_by_email(email):
+    """ Return a password by user email """
+
+    user = User.query.filter(User.email==email).first()
+    
+    return user.password
+
+def get_user_id_by_email(email):
+    """ Return user id by user email"""
+
+    user = User.query.filter(User.email==email).first()
+
+    return user.user_id
+
 def create_movie(title, overview, release_date, poster_path):
     """Create and return a new movie"""
 
@@ -19,14 +57,41 @@ def create_movie(title, overview, release_date, poster_path):
     return movie
 
 
+def show_all_movies():
+    """Return a list of all movies"""
+
+    movies = Movie.query.all()
+
+    return movies
+
+
+def get_movie_by_id(movie_id):
+    """ Return a movie by its id """
+
+    movie = Movie.query.get(movie_id)
+
+    return movie
+
+
+def get_movie_by_name(name):
+    """ Return a movie by its name """
+
+    movie = Movie.query.filter(Movie.title==name).first()
+
+    return movie
+
+
+
+
 def create_rating(user, movie, score):
     """ Create and return a rating"""
 
     rating = Rating(user=user, movie=movie, score=score)
 
     return rating
-
 # 2 ways: create new user and movie, or get existing user and movie
+
+
 
 
 
--- server.py
+++ server.py
@@ -1,14 +1,120 @@
 """Server for movie ratings app."""
 
-from flask import Flask
+from flask import (Flask, render_template, request, flash, session,
+                   redirect)
+from model import connect_to_db, db
+import crud
+from jinja2 import StrictUndefined
 
 app = Flask(__name__)
+app.secret_key = "dev"
+app.jinja_env.undefined = StrictUndefined
 
 
 # Replace this with routes and view functions!
+@app.route('/')
+def show_homepage():
+    """ Show homepage """
+
+    return render_template('homepage.html')
+
+
+@app.route('/movies')
+def show_movies():
+    """ Show all movies """
+
+    movies = crud.show_all_movies()
+
+    return render_template("all_movies.html", movies=movies)
+
+
+@app.route('/movies/<movie_id>')
+def show_movie_details(movie_id):
+    """ Show details about a movie """
+
+    movie = crud.get_movie_by_id(movie_id)
+
+    return render_template("movie_details.html", movie=movie)
+
+
+@app.route('/users')
+def show_users():
+    """ Show all users """
+
+    users = crud.show_all_users()
+
+    return render_template("users.html", users = users)
+
+
+@app.route('/users/<user_id>')
+def show_user_detail(user_id):
+    """ Show details about a user """
+
+    user = crud.user_by_id(user_id)
+
+    return render_template("user_details.html", user=user)
+
+
+@app.route('/new_users', methods = ['POST'])
+def register_user():
+    """ Create a new user """
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    user = crud.get_user_by_email(email)
+    if user:
+        flash('This email has already been used. Try a different email.')
+    else:
+        new_user = crud.create_user(email, password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Your account has been successfully created. You can log in now')
+
+    return redirect('/')
+
+
+@app.route('/login', methods = ['POST'])
+def login():
+    """ Login with email and password """
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    user_password = crud.get_password_by_email(email)
+
+    if user_password == password:
+        session['user'] = crud.get_user_id_by_email(email)
+        flash('Logged in!')
+    else:
+        flash('Incorrect password, please try again!')
+
+    return redirect('/')
+
+
+@app.route('/rate_movie', methods = ['POST'])
+def rate_a_movie():
+    """ Rate a movie """
+
+    movie_name = request.form.get('movie_name')
+    score = request.form.get('score')
+    user = crud.user_by_id(session['user'])
+
+    movie = crud.get_movie_by_name(movie_name)
+
+    if movie:
+        new_rating = crud.create_rating(user, movie, score)
+        db.session.add(new_rating)
+        db.session.commit()
+        flash('You successfully rated a movie!')
+    else:
+        flash('Sorry this movie is not in our list. Please choose a different movie to rate')
+
+    return redirect ('/')
+
 
 
 if __name__ == "__main__":
     # DebugToolbarExtension(app)
-
+    connect_to_db(app)
     app.run(host="0.0.0.0", debug=True)
